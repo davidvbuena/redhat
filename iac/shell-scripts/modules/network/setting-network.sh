7f2e1382aed785db1a019ec4f11cb95d760c8dba
David <david.buena@dbuena.com>
2023-05-09 00:29:56 -0300

    On branch get-started/0.0.1  Your branch is up to date with 'origin/get-started/0.0.1'.

--- iac/shell-scripts/modules/network/setting-network.sh
+++
@@ -1,5 +0,0 @@
-################## ssh root user
-sudo su -
-echo -e "vagrant\nvagrant" | passwd root
-    sed -in 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
-	service ssh restart
